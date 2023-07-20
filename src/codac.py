d62fa8a285e1410dfb80a5a002893941391072e2
rafalm11 <133100213+rafalm11@users.noreply.github.com>
2023-07-20 22:48:59 +0200

    initial getParameters

---
+++ src/codac.py
@@ -0,0 +1,25 @@
+#codac app main module
+
+from argparse import ArgumentParser
+from sys import argv
+
+def getParameters(params):
+    #function takes parameters list to ease testing
+    parser = ArgumentParser(
+        prog='CodacJoiner',
+        description='joins customer personal data with accounts')
+    parser.add_argument('-pf', '-personalFile', dest='personalFileName', type=str, help="consumer's personal data file (csv)", required=True, metavar='fileName')
+    parser.add_argument('-af', '-accountsFile', dest='accountsFileName', type=str, help="consumer's accounts data file (csv)", required=True, metavar='fileName')
+    parser.add_argument('-c', '-countryFilter', dest='countryFilter', type=str, 
+        help='list of countries to filter by', required=False, default ='Netherlands', metavar='countryName', nargs='*')
+    args = parser.parse_args(params)
+    return args.personalFileName, args.accountsFileName, args.countryFilter
+
+def main():
+    personalFileName, accountsFileName, countryFilter  = getParameters(argv[1:])
+    print (personalFileName)
+    print (accountsFileName)
+    print (countryFilter)
+
+if __name__ == '__main__':
+    main()
