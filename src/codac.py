31a39cf5c867cadc60e8b371d107b5c057682601
rafalm11 <133100213+rafalm11@users.noreply.github.com>
2023-07-21 13:03:17 +0200

    added -o and -s parameters

--- src/codac.py
+++ src/codac.py
@@ -8,18 +8,25 @@ def getParameters(params):
     parser = ArgumentParser(
         prog='codac.py',
         description='joins customer personal data with its accounts')
-    parser.add_argument('-pf', '-personalFile', dest='personalFileName', type=str, help="consumer's personal data file (csv)", required=True, metavar='fileName')
-    parser.add_argument('-af', '-accountsFile', dest='accountsFileName', type=str, help="consumer's accounts data file (csv)", required=True, metavar='fileName')
+    parser.add_argument('-p', '-personalFile', dest='personalFileName', type=str, help="consumer's personal data file (csv)", required=True, metavar='<fileName>')
+    parser.add_argument('-a', '-accountsFile', dest='accountsFileName', type=str, help="consumer's accounts data file (csv)", required=True, metavar='<fileName>')
+    parser.add_argument('-o', '-outputFolder', dest='outputFolderName', type=str, help="output data folder (will be overwritten. default:client_data)", 
+        required=False, metavar='<folderName>', default='client_data')
     parser.add_argument('-c', '-countryFilter', dest='countryFilter', type=str, 
-        help='list of countries to filter by (default: Netherlands)', required=False, default ='Netherlands', metavar='countryName', nargs='*')
+        help='list of countries to filter by (default: Netherlands)', required=False, default ='Netherlands', metavar='<countryName>', nargs='*')
+    parser.add_argument('-s', '-sparkUrl', dest='sparkUrl', type=str, 
+        help='spark instance url (default: local)', required=False, default ='local', metavar='<url>')
+    
     args = parser.parse_args(params)
-    return args.personalFileName, args.accountsFileName, args.countryFilter
+    return args.personalFileName, args.accountsFileName, args.outputFolderName, args.countryFilter, args.sparkUrl
 
 def main():
-    personalFileName, accountsFileName, countryFilter  = getParameters(argv[1:])
+    personalFileName, accountsFileName, outputFolderName, countryFilter, sparkUrl  = getParameters(argv[1:])
     print (personalFileName)
     print (accountsFileName)
+    print (outputFolderName)
     print (countryFilter)
+    print (sparkUrl)
 
 if __name__ == '__main__':
     main()
--- tests/test_codac.py
+++ tests/test_codac.py
@@ -1,14 +1,20 @@
 import pytest
 from src.codac import getParameters
 
-def test_parameters_3_countries():
-    assert getParameters(['-pf','aaa.csv','-af','bbb.csv','-c','c1','c2','c3'])==('aaa.csv','bbb.csv',['c1','c2','c3'])
+def test_parameters_output_file():
+    assert getParameters(['-p','aaa.csv','-a','bbb.csv','-o','testOutput'])==('aaa.csv','bbb.csv','testOutput','Netherlands','local')
 
-def test_parameters_none_countries():
-    assert getParameters(['-pf','aaa.csv','-af','bbb.csv'])==('aaa.csv','bbb.csv','Netherlands')
+def test_parameters_3_countries():
+    assert getParameters(['-p','aaa.csv','-a','bbb.csv','-c','c1','c2','c3'])==('aaa.csv','bbb.csv','client_data',['c1','c2','c3'],'local')
+
+def test_parameters_url():
+    assert getParameters(['-p','aaa.csv','-a','bbb.csv','-s','test.spark.url'])==('aaa.csv','bbb.csv','client_data','Netherlands','test.spark.url')
+
+def test_parameters_default_values():
+    assert getParameters(['-p','aaa.csv','-a','bbb.csv'])==('aaa.csv','bbb.csv','client_data','Netherlands','local')
 
 def test_parameters_displaced_countries():
-    assert getParameters(['-c','c1','c2','c3','-pf','aaa.csv','-af','bbb.csv'])==('aaa.csv','bbb.csv',['c1','c2','c3'])
+    assert getParameters(['-c','c1','c2','c3','-p','aaa.csv','-a','bbb.csv'])==('aaa.csv','bbb.csv', 'client_data', ['c1','c2','c3'],'local')
 
 def test_missing_file_names():
     with pytest.raises(SystemExit) as info: 
