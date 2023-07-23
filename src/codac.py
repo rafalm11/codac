a9a9bd7e3e84b18badb8808f58aba91d43f8d28e
rafalm11 <133100213+rafalm11@users.noreply.github.com>
2023-07-24 00:23:13 +0200

    generic function for filter and rename

--- src/codac.py
+++ src/codac.py
@@ -44,6 +44,21 @@ def loggerInit(loggerName: str):
     
 logger = loggerInit(__name__)
 
+from pyspark.sql import DataFrame
+from typing import Dict, List
+
+def customFilter(df: DataFrame, filterMap: Dict[str, List[str]]) -> DataFrame:
+    logger.debug('customFilter started. filter:'+str(filterMap))
+    for column,valuesList in filterMap.items():
+        df = df.filter(df[column].isin(valuesList))
+        logger.debug('filter by column:'+column+' with values:'+str(valuesList))
+    return df
+
+def customRename(df: DataFrame, renameMap: Dict[str,str]) -> DataFrame:
+    logger.debug('customRename started. renameMap:'+str(renameMap))
+    df = df.withColumnsRenamed(renameMap)
+    return df
+
 
 
 def main():
@@ -63,7 +78,7 @@ def main():
     dfOut = dfPersonal.select('id','email','country')
     logger.debug('created Out Dataframe with removed columns from Personal DataFrame')
 
-    dfOut = dfOut.filter(dfPersonal.country.isin(countryFilter))
+    dfOut =  customFilter(dfOut,{'country':countryFilter}) #dfOut.filter(dfPersonal.country.isin(countryFilter))
     logger.debug('filtered columns on Out DataFrame')
 
     dfOut = dfOut.join(dfAccounts,on=dfPersonal.id==dfAccounts.id,how='inner')
@@ -72,7 +87,7 @@ def main():
     dfOut = dfOut.drop(dfAccounts.id)
     logger.debug('removed duplicated join column from Out Dataframe')
 
-    dfOut = dfOut.withColumnsRenamed({'id':'client_identifier','btc_a':'bitcoin_address','cc_t':'credit_card_type'})
+    dfOut = customRename(dfOut,{'id':'client_identifier','btc_a':'bitcoin_address','cc_t':'credit_card_type'}) #dfOut.withColumnsRenamed({'id':'client_identifier','btc_a':'bitcoin_address','cc_t':'credit_card_type'})
     logger.debug('renamed columns on Out DataFrame')
 
     logger.debug('dfOut count:'+str(dfOut.count()))
--- tests/test_codac.py
+++ tests/test_codac.py
@@ -1,4 +1,6 @@
 import pytest
+
+#test getParameters
 from src.codac import getParameters
 
 def test_parameters_output_file():
@@ -21,7 +23,46 @@ def test_missing_file_names():
         getParameters(['-c','c1'])
 
 
-
+# test logging
 def test_log(caplog):
     getParameters(['-p','aaa.csv','-a','bbb.csv'])
     assert 'getParameters started' in caplog.text
+
+
+# test pyspark 
+import chispa
+from pyspark.sql import SparkSession
+from src.codac import customFilter, customRename
+
+@pytest.fixture
+def spark():
+    return SparkSession.builder.master('local').getOrCreate()
+
+@pytest.fixture
+def sourceData():
+    return[('name1','surname1','country1'),
+            ('name2','surname2','country1'),
+            ('name3','surname3','country3'),
+            ('name4','surname4','country4')]
+@pytest.fixture
+def sourceColumns():
+    return['name','surname','country']
+
+@pytest.fixture
+def dfSource(spark,sourceData,sourceColumns):
+    return spark.createDataFrame(sourceData,sourceColumns)
+
+def test_customFilter_1country(spark,dfSource):
+    dfExpected = spark.createDataFrame([('name1','surname1','country1'),
+                                ('name2','surname2','country1'),                                
+                                ('name4','surname4','country4')],['name','surname','country'])
+    chispa.assert_df_equality(customFilter(dfSource,{'country':['country1','country4']}),dfExpected)
+
+def test_custom_filter_2countries(spark,dfSource):
+    dfExpected = spark.createDataFrame([('name1','surname1','country1'),
+                                ('name2','surname2','country1')],['name','surname','country'])
+    chispa.assert_df_equality(customFilter(dfSource,{'country':'country1'}),dfExpected)
+
+def test_customRename(spark,sourceData,sourceColumns, dfSource):
+    dfExpected = spark.createDataFrame(sourceData,['firstname','lastname','country'])
+    chispa.assert_df_equality(customRename(df=dfSource, renameMap={'name':'firstname','surname':'lastname'}),dfExpected)
